a9f634242206fee3a3b2e99c6f33415ba4453959
Colin Warner <colin.x.warner@gmail.com>
2020-12-22 10:20:28 -0600

    conditionals exercise

--- js/conditionals.js
+++ js/conditionals.js
@@ -9,19 +9,19 @@ console.log ("Conditionals Exercises")
  // * color. Only worry about the colors defined below, if the color passed is not
  // * one of the ones defined below, return a message that says so
  // *
-    var colorChoice = prompt("What color are you thinking of right now?");
-
-    function analyzeColor(colorChoice) {
-            if (colorChoice === "blue") {
-                return ("blue is the color of the sky.");
-            } else if (colorChoice === "red") {
-                return ("Strawberries are red.");
-            } else if (colorChoice === "cyan") {
-                return ("I don't know anything about cyan.");
-            } else {
-                return ("I've never heard of " + colorChoice + " so, you must have made it up.")
-            }
-    }
+ //    var colorChoice = prompt("What color are you thinking of right now?");
+ //
+ //    function analyzeColor(colorChoice) {
+ //            if (colorChoice === "blue") {
+ //                return ("blue is the color of the sky.");
+ //            } else if (colorChoice === "red") {
+ //                return ("Strawberries are red.");
+ //            } else if (colorChoice === "cyan") {
+ //                return ("I don't know anything about cyan.");
+ //            } else {
+ //                return ("I've never heard of " + colorChoice + " so, you must have made it up.")
+ //            }
+ //    }
  // * Example:
  // *  > analyzeColor('blue') // returns "blue is the color of the sky"
  // *  > analyzeColor('red') // returns "Strawberries are red"
@@ -56,35 +56,40 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
 //  * TODO:
 //  * Refactor your above function to use a switch-case statement
 //  */
-function analyzeColorSwitch(colorChoice) {
-    switch (colorChoice) {
-        case "blue":
-            return ("blue is the color of the sky.");
-        case "red":
-            return ("Strawberries are red.");
-        case "cyan":
-            return ("I don't know anything about cyan.");
-        default:
-            return ("I've never heard of " + colorChoice + " so, you must have made it up.")
-    }
-}
-   console.log(analyzeColorSwitch('blue'), "blue is the color of the sky.");
-   console.log(analyzeColorSwitch('red'), "Strawberries are red.");
-   console.log(analyzeColorSwitch('cyan'), "I don't know anything about cyan");
-   console.log(analyzeColorSwitch('colorChoice'), "I've never heard of " + colorChoice + " so, you must have made it up.");
+// function analyzeColorSwitch(colorChoice) {
+//     switch (colorChoice) {
+//         case "blue":
+//             return ("blue is the color of the sky.");
+//         case "red":
+//             return ("Strawberries are red.");
+//         case "cyan":
+//             return ("I don't know anything about cyan.");
+//         default:
+//             return ("I've never heard of " + colorChoice + " so, you must have made it up.")
+//     }
+// }
+//    console.log(analyzeColorSwitch('blue'), "blue is the color of the sky.");
+//    console.log(analyzeColorSwitch('red'), "Strawberries are red.");
+//    console.log(analyzeColorSwitch('cyan'), "I don't know anything about cyan");
+//    console.log(analyzeColorSwitch('colorChoice'), "I've never heard of " + colorChoice + " so, you must have made it up.");
 // EXAMPLE SWITCH-CASE STATEMENT WITH BREAKS FROM JUSTIN'S YOUTUBE LECTURE//
-function analyzeColorSwitchWithBreak(colorChoice) {
-    switch (colorChoice) {
-        case "blue":
-            return ("blue is the color of the sky.");
-        case "red":
-            return ("Strawberries are red.");
-        case "cyan":
-            return ("I don't know anything about cyan.");
-        default:
-            return ("I've never heard of " + colorChoice + " so, you must have made it up.")
-    }
-}
+// function analyzeColorSwitchWithBreak(colorChoice) {
+//     var colorMsg
+//     switch (colorChoice) {
+//         case "blue":
+//             colorMsg = "blue is the color of the sky.";
+//              break;
+//         case "red":
+//             colorMsg = "Strawberries are red.";
+//              break;
+//         case "cyan":
+//             colorMsg = "I don't know anything about cyan.";
+//              break;
+//         default:
+//             return ("I've never heard of " + colorChoice + " so, you must have made it up.")
+//     }
+//      return colorMsg;
+// }
 
 
 // /**
@@ -93,28 +98,57 @@ function analyzeColorSwitchWithBreak(colorChoice) {
 //  * user to your `analyzeColor` function. Alert the return value from your
 //  * function to show it to the user.
 //  */
+// var colorChoice = prompt("What color are you thinking of right now?").toLowerCase();
+// alert (analyzeColor(colorChoice));
 
 /* ########################################################################## */
-
-/**
- * TODO:
- * Suppose there's a promotion in Walmart, each customer is given a randomly
- * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
- * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
- * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
- * all for free!.
- *
- * Write a function named `calculateTotal` that accepts a lucky number and total
- * amount, and returns the discounted price.
- *
- * Example:
- * calculateTotal(0, 100) // returns 100
- * calculateTotal(4, 100) // returns 50
- * calculateTotal(5, 100) // returns 0
- *
- * Test your function by passing it various values and checking for the expected
- * return value.
- */
+//
+// /**
+//  * TODO:
+//  * Suppose there's a promotion in Walmart, each customer is given a randomly
+//  * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
+//  * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
+//  * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
+//  * all for free!.
+//  *
+//  * Write a function named `calculateTotal` that accepts a lucky number and total
+//  * amount, and returns the discounted price.
+//  *
+//  * Example:
+//  * calculateTotal(0, 100) // returns 100
+//  * calculateTotal(4, 100) // returns 50
+//  * calculateTotal(5, 100) // returns 0
+//  *
+//  * Test your function by passing it various values and checking for the expected
+//  * return value.
+//  */
+function calculateTotal(luckyNumber, totalAmount) {
+    var discountNumber;
+    switch (luckyNumber) {
+        case 0:
+            discountNumber = 0;
+            break;
+        case 1:
+            discountNumber = .1;
+            break;
+        case 2:
+            discountNumber = .25;
+            break;
+        case 3:
+            discountNumber = .35;
+            break;
+        case 4:
+            discountNumber = .5;
+            break;
+        case 5:
+            discountNumber = 1;
+            break;
+    }
+    return totalAmount - (discountNumber * totalAmount)
+}
+console.log(calculateTotal(0, 100)); 100 // returns 100
+console.log(calculateTotal(4, 100)); 50 // returns 50
+console.log(calculateTotal(5, 100)); 0 // returns 0
 
 /**
  * TODO:
