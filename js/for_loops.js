564b5832040f9a3e3b323621b8b9b60209291fd1
Colin Warner <colin.x.warner@gmail.com>
2021-01-04 23:09:44 -0600

    Refactored and completed while.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -12,12 +12,12 @@
 // 2. Create a function named showMultiplicationTable that
 // accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
 
-// function showMultiplicationTable(num) {
-//     // create a loop that runs 10x
-//     for (var i = 1; i < 11; i += 1) {
-//         console.log(num + " x " + i + "=" + i * num);
-//     }
-// }
+function showMultiplicationTable(num) {
+    // create a loop that runs 10x
+    for (var i = 1; i < 11; i += 1) {
+        console.log(num + " x " + i + "=" + i * num);
+    }
+}
 
 //showMultiplicationTable(7);
 
@@ -43,18 +43,18 @@
 // ...
 
 // src - https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-// function randomIntFromInterval(min, max) { // min and max included
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-// }
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 //
-// function createTenRandomEvenOddMessages() {
-//     var random, evenOddString;
-//     for (var i = 0; i < 10; i += 1) {
-//         random = randomIntFromInterval(20, 200);
-//         evenOddString = (random % 2 === 0) ? "even" : "odd";
-//         console.log(random + " is " + evenOddString);
-//     }
-// }
+function createTenRandomEvenOddMessages() {
+    var random, evenOddString;
+    for (var i = 0; i < 10; i += 1) {
+        random = randomIntFromInterval(20, 200);
+        evenOddString = (random % 2 === 0) ? "even" : "odd";
+        console.log(random + " is " + evenOddString);
+    }
+}
 
 //createTenRandomEvenOddMessages();
 
@@ -95,7 +95,7 @@
 // 10
 // 5
 
-for (var i = 100; i > 0; i -= 5 ); {
-    console.log(i);
-}
+// for (var i = 100; i > 0; i -= 5 ); {
+//     console.log(i);
+// }
 
--- js/while.js
+++ js/while.js
@@ -21,6 +21,12 @@
 // 32768
 // 65536
 
+// var num = 2;
+// while (num <= 65536) {
+//     console.log(num);
+//     num *= 2;
+// }
+
 // Do While Loop
 // 1. An ice cream seller can't go home until she sells all of her cones.
 //    First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell
@@ -30,8 +36,8 @@
 //    Use a do-while loop to log to the console the amount of cones sold to each person.
 //    This is a way get the random numbers for this exercise.
 // This is how you get a random number between 50 and 100
-
 // var allCones = Math.floor(Math.random() * 50) + 50;
+
 // // This expression will generate a random number between 1 and 5
 // Math.floor(Math.random() * 5) + 1;
 
@@ -40,3 +46,44 @@
 // 5 cones sold...  // if there are enough cones
 // Cannot sell you 6 cones I only have 3...  // If there are not enough cones
 // Yay! I sold them all! // If there are no more cones
+
+//======================================ICE CREAM EXERCISE
+
+
+
+function pluralOrSingularCones(numOfCones) {
+    return (numOfCones > 1) ? "cones" : "cone";
+}
+
+function createRandomIceCreamOrder(allCones) {
+    var conesToSell;
+
+    // create random (var conesToSell) number of cones to sell
+    conesToSell = Math.floor(Math.random() * 5) + 1;
+
+    // if random (var conesToSell) is <= cones left decrease allCones by random (var conesToSell)
+    if (conesToSell <= allCones) {
+        console.log(conesToSell + ' ' + pluralOrSingularCones(conesToSell) + " sold..."); //log random (var conesToSell) cones sold
+        allCones -= conesToSell; //subtract conesToSell from allCones
+    } else { //otherwise
+        console.log("Cannot sell you " + conesToSell + ' ' + pluralOrSingularCones(conesToSell) + " I only have " + allCones + " ..."); //log cannot sell message
+    }
+    console.log(allCones + " are left");
+    return allCones;
+}
+
+
+function runIceCreamStore() {
+
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    console.log("We have " + allCones + " to sell");
+
+    do {
+        allCones = createRandomIceCreamOrder(allCones);
+    } while (allCones > 0);
+
+    console.log("Yay! I sold them all");
+}
+
+
+runIceCreamStore();
