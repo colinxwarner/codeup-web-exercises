4d33b08c303bacf63b054508ba3a10c33ad307ae
Colin Warner <colin.x.warner@gmail.com>
2021-03-08 10:54:09 -0600

    Adding previous dom review

---
+++ js/dom-review.js
@@ -0,0 +1,57 @@
+
+// ================= Review, pt deux =================
+
+/*
+    TODO: When a list item inside of .data-example-container is clicked,
+     -return its data-value attribute value to the element with an id of #show-data-attr
+*/
+    $(".data-example-container").children().click(function (){
+        console.log();
+
+        $(this)
+    })
+/*
+    TODO: When a list item inside of .data-example-container is double clicked,
+     -return its text value to the all elements with a class of .show-list-item-text
+ */
+
+/*
+
+    TODO: When an immediate child element of the parent with an id of #hover-container is hovered
+     -change the child's text to 'You are hovering here!' and
+     -add a border to the child.
+     -Be sure to reset the text and border upon hovering out
+
+ */
+
+/*
+
+    TODO: When an element with the class of .background-color-change is clicked,(changes colors with each click)
+     -cycle between background-colors of 'red', 'blue', 'green', and back to the default
+     -the colors should change in the above order
+     -only affect one element at a time
+
+ */
+
+/*
+
+    TODO: When a user enters a string (and only a string) into the input of id #input,
+     -concatenate that string to what is already in the element with an id of #output
+     -when the user double clicks the element with an id #output
+     -the string resets to its original text
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
